f16053c21261796345445825f0aa276d81b41d6d
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 20:15:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
+  count = 0 
+  while count < array.length do
+    return array.index(value_to_find)
+  end
+  nil 
+end 
 
 def find_max_value(array)
   # Add your solution here
+
+
+
 end
 
 def find_min_value(array)
